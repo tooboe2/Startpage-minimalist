2c827c696bcec6112e11d5c405e8c9a77be1aab1
smezzy <yagamiztc123@gmail.com>
2021-08-12 17:28:33 -0300

    removi o cringe

--- main.js
+++
@@ -1,8 +0,0 @@
-var pesquisar = document.getElementById('pesquisar');
-pesquisar.addEventListener("keydown", (e) =>{
-    if(e.keyCode == 13){
-        var termos = pesquisar.value;
-        window.open(`https://www.google.com/search?q=${termos}`);
-        pesquisar.value = "";
-    }
-})
